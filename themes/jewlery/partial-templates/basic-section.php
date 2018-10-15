1e1a9ff9e84c66ba386c85f0ccd90ccf0b86b00e
Kevin Sanabria <sanabriakevin@hotmail.com>
2018-10-15 18:41:06 -0400

    have image stick to bottom if div is of the team

--- themes/jewlery/partial-templates/basic-section.php
+++ themes/jewlery/partial-templates/basic-section.php
@@ -6,17 +6,18 @@ if (have_rows('basic_section')):?>
     $section_image = get_sub_field('section_image');
     $image_src = $section_image['sizes']['large'];
     $image_alt = ($section_image['alt'] == "" ? 'section-image' : $section_image['alt']);
+    $image_center = $image_alt !== "chris_sher" ? 'align-item-center' : '';
     $image_position_class = (get_sub_field('image_position') == 'left' ? 'flex-first' : 'flex-lg-last flex-first');
-    
+
 ?>
     <article class="basic-section">
         <div class="inner-wrapper">
             <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 col-12 <?php echo $image_position_class;?>">
-                        <img class="basic-section-image" src="<?php echo $image_src;?>" alt="<?php echo $image_alt;?>"/>
+                <div class="row d-flex <?php echo $image_center ?>" id="<?php echo $image_alt;?>">
+                    <div class="col-lg-6 col-12 <?php echo $image_position_class;?> <?php echo $image_alt;?>" >
+                            <img class="basic-section-image"  id="<?php echo $image_alt ?>" src="<?php echo $image_src;?>" alt="<?php echo $image_alt;?>"/>
                     </div>
-                    <div class="col-lg-6 col-12">
+                    <div class="col-lg-6 col-12 <?php echo $image_alt;?>_text">
                         <h2 class="section-title text-center"><?php the_sub_field('section_heading');?> </h2>
                         <div class="section-content text-center">
                             <?php the_sub_field('section_content');?>
